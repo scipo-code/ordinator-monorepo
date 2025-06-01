146c6c10442ee14b3b3181ce85c4a45b927552bc
sebdal <semoda@bio.aau.dk>
2025-06-01 13:23:27 +0000

    changed backend port

--- static_files/scheduler/vite.config.ts
+++ static_files/scheduler/vite.config.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,8 +18,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'http://localhost:8090',
-        changeOrigin: true
+        target: "http://localhost:3000",
+        changeOrigin: true,
       },
     },
   },
