a307b702c52e5aac23abc666d793a852a7579ff8
cbrje <chri1423@gmail.com>
2025-06-22 20:08:54 +0000

    work in progress: setting up the debugger for Rust

--- .lldb/chrono.py
+++ .lldb/chrono.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 
 def DateTimeSummary(valobj, internal_dict):
-    ymdf = valobj.GetChildMemberWithName('datetime').GetChildMemberWithName('date').GetChildMemberWithName('ymdf').GetValue()
-    secs = valobj.GetChildMemberWithName('datetime').GetChildMemberWithName('time').GetChildMemberWithName('time').GetValue()
+    ymdf = valobj.GetChildMemberWithName('datetime').GetChildMemberWithName('date').GetChildMemberWithName('yof').GetChildMemberWithName('__0').GetChildMemberWithName('0').GetValueAsUnsigned();
+    secs = valobj.GetChildMemberWithName('datetime').GetChildMemberWithName('time').GetChildMemberWithName('secs').GetValueAsUnsigned();
     year = int(ymdf) >> 13
-    day_of_year = (int(ymdf) & 8191 >> 4;
-    date = datetime(year -1, 12, 31) + timedelta(days=day_of_year) + timedelta(seconds=int(secs))
+    day_of_year = (int(ymdf) & 8191) >> 4;
+    date = datetime(year - 1, 12, 31) + timedelta(days=day_of_year) + timedelta(seconds=int(secs))
     return date.strftime('%Y-%m-%d %H:%M:%S')
