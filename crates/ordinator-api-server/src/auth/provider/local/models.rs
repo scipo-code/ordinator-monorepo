16a3b97d8b58bc64f8a6e067386043b3a799784f
sebdal <semoda@bio.aau.dk>
2026-01-11 08:23:15 +0000

    moved to contracts

--- crates/ordinator-api-server/src/auth/provider/local/models.rs
+++ crates/ordinator-api-server/src/auth/provider/local/models.rs
@@ -1,9 +1,1 @@
-use serde::Deserialize;
-use utoipa::ToSchema;
 
-#[derive(Debug, Deserialize, ToSchema)]
-pub struct LocalAuthPayload
-{
-    pub client_id: String,
-    pub client_secret: String,
-}
