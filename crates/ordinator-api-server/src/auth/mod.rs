bf03018124728d02ab39a93a7d3e80841266f14f
sebdal <semoda@bio.aau.dk>
2026-01-11 08:25:17 +0000

    clean up

--- crates/ordinator-api-server/src/auth/mod.rs
+++ crates/ordinator-api-server/src/auth/mod.rs
@@ -1,11 +1,10 @@
 use chrono::Utc;
 
 pub mod db;
-pub mod jwt;
+pub mod extractors;
 pub mod models;
 pub mod provider;
 pub mod routes;
-pub mod extractors;
 
 pub fn current_timestamp() -> i64
 {
