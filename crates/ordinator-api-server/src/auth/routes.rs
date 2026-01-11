1b4b530487bc090c46b928a9b2bd700a4f2c97f7
sebdal <semoda@bio.aau.dk>
2026-01-11 08:28:23 +0000

    added a auth_config path to allow frontend to figure out the authentication system
    The config is used because I think the best way for setting this
    application up is with ONE authentication system. It should have
    multiple authentication setups. Though we need a simple auth for testing
    whereas companies will have their own Azure AD, Google etc

--- crates/ordinator-api-server/src/auth/routes.rs
+++ crates/ordinator-api-server/src/auth/routes.rs
@@ -1,17 +1,11 @@
-use serde::Serialize;
+use axum::Json;
+use axum::extract::State;
+use ordinator_contracts::auth::AuthConfig;
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
 
 use crate::AppState;
 use crate::auth::provider::Provider;
-
-#[derive(Serialize)]
-pub struct LoginResponse
-{
-    pub access_token: String,
-    pub refresh_token: String,
-    pub token_type: String,
-}
 
 pub async fn authentication_nest(state: AppState) -> OpenApiRouter<AppState>
 {
@@ -24,5 +18,24 @@ pub async fn authentication_nest(state: AppState) -> OpenApiRouter<AppState>
 
         Provider::Azure => todo!(),
     }
+    .routes(routes!(crate::auth::routes::auth_config))
     .with_state(state)
+}
+
+#[utoipa::path(
+    get,
+    tag = "Authentication",
+    path = "/config",
+    responses(
+        (status = 200),
+    )
+)]
+pub async fn auth_config(State(state): State<AppState>) -> Json<AuthConfig>
+{
+    // At some point if we do SaaS we would need to have multitenant support.
+    // This would need to be refactored to check the user email and create the
+    // appropriate response
+    Json(AuthConfig {
+        provider: state.config.auth_provider.to_string(),
+    })
 }
